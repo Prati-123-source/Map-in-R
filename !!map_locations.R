876000c6944ec5d3514f5d7c3522b7fcccd5fab0
Prati-123-source <123968221+Prati-123-source@users.noreply.github.com>
2023-02-20 13:06:31 +0100

    Add files via upload

---
+++ !!map_locations.R
@@ -0,0 +1,76 @@
+####################################################
+# Get Locations into a map
+# nach folgender Anleitung:  https://www.youtube.com/watch?v=2k8O-Y_uiRU
+# Need to be online to run the code!
+###################################################
+
+rm(list=ls())
+
+version
+#libraries needed
+#####################
+library(tidyverse)
+library(googlesheets4)
+library(lubridate)
+library(openintro)
+# answer no at the following prompt: there is no compiler here...
+# install.packages("sp", dependencies=TRUE)
+library(sp)
+# install.packages("ggmap")
+library(ggmap)
+library(readxl)
+library(ggrepel)
+library (giscoR)
+library(sf)
+library(scales)
+#######################
+# get coordinates from openstreetmap.org mit der Exportfunktion
+
+Gera_map = get_stamenmap(
+    bbox = c(left = 13.3324, bottom = 54.0556, right = 13.5068, top =54.1130),
+    maptype = "terrain",
+    zoom = 13
+)
+ggmap(Gera_map)
+#############
+
+# Read in the file with locations:
+
+
+dir()
+excel_sheets("Locations.xlsx")
+locations = read_excel("Locations.xlsx", sheet = "Sheet1")
+str(locations)
+locations$Site
+list(locations)
+# plot locations in map
+
+
+
+coords <- data.frame(lat = c(54.11, 50.6),
+                     long = c(13.33, 13.51))
+
+ggmap(Gera_map) + 
+    geom_point(data = coords,
+               aes(x = long, y = lat), 
+               size = 2) +coord_equal()  + 
+    ggsn::north(x.min = 13.34, x.max = 13.35, 
+                y.min = 54.11, y.max = 54.10, scale = 1.5) + 
+    ggsn::scalebar(x.min = 13.33, x.max = 13.38, 
+                   y.min = 54.11, y.max = 54.11, 
+                   dist = 2, transform= TRUE, dist_unit="km",
+                   model = "WGS84", height = 0.5, 
+                   st.dist = 0.5)
+
+n<-ggmap(Gera_map)+geom_point(data=locations, aes(x=long, y=lat))+ 
+    ggsn::scalebar(x.min = 13.44, x.max = 13.50, 
+                   y.min = 54.06, y.max = 54.06, 
+                   dist = 1,dist_unit = "km", transform = TRUE, 
+                   model = "WGS84", height = 0.5, 
+                   st.dist = 0.5) 
+north2(n, x=.95, y=.85, symbol=9)
+
+
+n1<-ggmap(Gera_map)+geom_point(data=locations, aes(x=long, y=lat, size=bat_activity, fill=bat_activity), shape=21, alpha=0.8)+ggsn::scalebar(x.min = 13.44, x.max = 13.50, y.min = 54.06, y.max = 54.06, dist = 1,dist_unit = "km", transform = TRUE,  model = "WGS84", height = 0.5,  st.dist = 0.5) 
+north2(n1, x=.8, y=.8, symbol=9)
+
